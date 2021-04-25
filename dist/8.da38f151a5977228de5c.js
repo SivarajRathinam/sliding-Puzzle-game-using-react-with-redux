fb7abdf9ac4cefda83dfdaf1c465d302695463d1
SivarajRathinam <sivaraj.mca17@gmail.com>
2021-04-25 16:31:16 +0530

    updating build files

---
+++ dist/8.da38f151a5977228de5c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{49:function(e,a,t){"use strict";t.r(a);var n=t(0),c=t.n(n),r=t(10);a.default=Object(r.b)(function(e){return{total:e.total}})(function(e){return c.a.createElement("div",{className:"Score"},c.a.createElement("span",{className:"score-header"},"Score"),c.a.createElement("span",{className:"Score-Number"},e.total))})}}]);
