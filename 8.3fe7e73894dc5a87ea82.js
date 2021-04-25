5e3bc7546b944f259de3d61fa0a2a07ae07496da
SivarajRathinam <sivaraj.mca17@gmail.com>
2021-04-25 16:30:38 +0530

    Updates

--- 8.3fe7e73894dc5a87ea82.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{42:function(e,a,t){"use strict";t.r(a);var n=t(0),c=t.n(n),r=t(9);a.default=Object(r.b)(function(e){return{total:e.total}})(function(e){return c.a.createElement("div",{className:"Score"},c.a.createElement("span",{className:"score-header"},"Score"),c.a.createElement("span",{className:"Score-Number"},e.total))})}}]);
